d107048f4fe6b11f6408e0dcfe4f9cfc2d100e10
umbrella-h <umbrella.hsiao@gmail.com>
2022-12-21 20:48:28 +0800

    feat(gemfile, lib): filepath argument & crop/upload image.

--- lib/client.rb
+++ lib/client.rb
@@ -1,3 +1,6 @@
 require_relative './use_cases/grab_avatar.rb'
+require_relative './use_cases/upload_image.rb'
 
-UseCases::GrabAvatar.new.perform
+filepath = './avatar.jpg'
+UseCases::GrabAvatar.new.perform(filepath: filepath)
+UseCases::UploadImage.new.perform(filepath: filepath, public_id: 'my_avatar')
--- lib/use_cases/grab_avatar.rb
+++ lib/use_cases/grab_avatar.rb
@@ -1,11 +1,20 @@
 require 'grabzit'
+require_relative './crop_image.rb'
 
 module UseCases
   class GrabAvatar
-    def perform
+    AVATAR_WIDTH = 140
+    AVATAR_HEIGHT = 147
+    BLEED = 50
+
+    def perform(filepath:)
       # TODO: timeout
       client.url_to_image(avatar_url, options)
       client.save_to(filepath)
+
+      # Sorry that I have to crop the watermark for layouts.
+      # I'll design another section to give credit to GrabzIt in my final layout.
+      CropImage.new.perform(filepath: filepath, width: AVATAR_WIDTH, height: AVATAR_HEIGHT)
     end
 
     private
@@ -13,7 +22,7 @@ module UseCases
     def options
       @options ||=
         image_options = GrabzIt::ImageOptions.new
-        set_browser_size(options: image_options, height: 147 + 50, width: 140)
+        set_browser_size(options: image_options, height: AVATAR_HEIGHT + BLEED, width: AVATAR_WIDTH)
         image_options
     end
 
@@ -28,10 +37,6 @@ module UseCases
 
     def avatar_url
       "https://crookedneighbor.github.io/habitica-avatar/avatar.html##{ENV['HABITICA_USER_ID']}"
-    end
-
-    def filepath
-      './avatar.jpg'
     end
   end
 end
